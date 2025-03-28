e20a851e41ef284d94fc622ba422143ae46b26b5
sebastianVis <sebaselnub@gmail.com>
2025-03-28 08:44:01 -0500

    project created

---
+++ ddl.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE IF EXISTS campusdatabase;
+USE campusdatabase;
+
+CREATE TABLE sedeCampus (
+
+);
+
+CREATE TABLE campers (
+
+);
+
+CREATE TABLE acudientes (
+
+);
+
+CREATE TABLE estado (
+
+);
+
+CREATE TABLE nivelRiesgo (
+
+);
+
+CREATE TABLE rutaEntrenamiento (
+
+); 
+
+CREATE TABLE evaluacion (
+
+);
+
+CREATE TABLE trainers (
+
+);
+
+CREATE TABLE AsignacionTrainer (
+
+);
+
+CREATE TABLE horarios (
+
+);
